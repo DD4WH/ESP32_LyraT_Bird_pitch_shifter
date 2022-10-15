28767a24ee5c18b30db88f4083c22c66a42b56cd
DD4WH <dd4wh@darc.de>
2022-10-16 00:15:16 +0200

    Add files via upload

--- main/main.c
+++ main/main.c
@@ -1,10 +1,11 @@
-// NEW 2022_10_14
+// NEW 2022_10_15
+//
 
 #include <esp_log.h>
 #include <driver/i2s.h>
 #include <driver/i2c.h>
 #include <math.h>
-//#include <esp_dsp.h>
+#include <esp_dsp.h>
 
 #include "es8388_registers.h"
 
@@ -21,10 +22,11 @@
 #define I2S_NUM I2S_NUM_0
 //#define I2S_READLEN 50 * 4
 //#define I2S_READLEN 192 * 4
-#define I2S_READLEN 252 * 4  // Maximum is 1024 == 256 * 4
+#define I2S_READLEN 128 * 4  // Maximum is 1024 == 256 * 4
 
 #define I2C_NUM I2C_NUM_0
 #define ES8388_ADDR 0x20
+
 
 
 /*
@@ -95,7 +97,7 @@ static esp_err_t es8388_init()
 	res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL1, 0x88);
 
 	/* select LINPUT2 / RINPUT2 as ADC input; stereo; 16 bit word length, format right-justified, MCLK / Fs = 256 */
-	res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL2, 0x50);
+	res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL2, 0x50); // 0x00 for built-in mics, 0x50 for AUX in
 	res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL3, 0x00);
 	res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL4, 0x0e);
 	res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL5, 0x02);
@@ -126,6 +128,125 @@ static inline int16_t dummyfilter(int16_t x)
 	return x;
 }
 
+
+float Izero (float x)
+{
+  float x2 = x / 2.0;
+  float summe = 1.0;
+  float ds = 1.0;
+  float di = 1.0;
+  float errorlimit = 1e-9;
+  float tmp;
+  do
+  {
+    tmp = x2 / di;
+    tmp *= tmp;
+    ds *= tmp;
+    summe += ds;
+    di += 1.0;
+  }   while (ds >= errorlimit * summe);
+  return (summe);
+}  // END Izero
+
+float m_sinc(int m, float fc)
+{ // fc is f_cut/(Fsamp/2)
+  // m is between -M and M step 2
+  //
+  float x = m * PIH;
+  if (m == 0)
+    return 1.0f;
+  else
+    return sinf(x * fc) / (fc * x);
+}
+
+void calc_FIR_coeffs (float * coeffs_I, int numCoeffs, float fc, float Astop, int type, float dfc, float Fsamprate)
+// pointer to coefficients variable, no. of coefficients to calculate, frequency where it happens, stopband attenuation in dB,
+// filter type, half-filter bandwidth (only for bandpass and notch)
+{ // modified by WMXZ and DD4WH after
+  // Wheatley, M. (2011): CuteSDR Technical Manual. www.metronix.com, pages 118 - 120, FIR with Kaiser-Bessel Window
+  // assess required number of coefficients by
+  //     numCoeffs = (Astop - 8.0) / (2.285 * TPI * normFtrans);
+  // selecting high-pass, numCoeffs is forced to an even number for better frequency response
+
+  float Beta;
+  float izb;
+  float fcf = fc;
+  int nc = numCoeffs;
+  fc = fc / Fsamprate;
+  dfc = dfc / Fsamprate;
+  // calculate Kaiser-Bessel window shape factor beta from stop-band attenuation
+  if (Astop < 20.96)
+    Beta = 0.0;
+  else if (Astop >= 50.0)
+    Beta = 0.1102 * (Astop - 8.71);
+  else
+    Beta = 0.5842 * powf((Astop - 20.96), 0.4) + 0.07886 * (Astop - 20.96);
+
+  for (int i = 0; i < numCoeffs; i++) //zero pad entire coefficient buffer, important for variables from DMAMEM
+  {
+    coeffs_I[i] = 0.0;
+  }
+
+  izb = Izero (Beta);
+  if (type == 0) // low pass filter
+    //     {  fcf = fc;
+  { fcf = fc * 2.0;
+    nc =  numCoeffs;
+  }
+  else if (type == 1) // high-pass filter
+  { fcf = -fc;
+    nc =  2 * (numCoeffs / 2);
+  }
+  else if ((type == 2) || (type == 3)) // band-pass filter
+  {
+    fcf = dfc;
+    nc =  2 * (numCoeffs / 2); // maybe not needed
+  }
+  else if (type == 4) // Hilbert transform
+  {
+    nc =  2 * (numCoeffs / 2);
+    // clear coefficients
+    for (int ii = 0; ii < 2 * (nc - 1); ii++) coeffs_I[ii] = 0;
+    // set real delay
+    coeffs_I[nc] = 1;
+
+    // set imaginary Hilbert coefficients
+    for (int ii = 1; ii < (nc + 1); ii += 2)
+    {
+      if (2 * ii == nc) continue;
+      float x = (float)(2 * ii - nc) / (float)nc;
+      float w = Izero(Beta * sqrtf(1.0f - x * x)) / izb; // Kaiser window
+      coeffs_I[2 * ii + 1] = 1.0f / (PIH * (float)(ii - nc / 2)) * w ;
+    }
+    return;
+  }
+
+  for (int ii = - nc, jj = 0; ii < nc; ii += 2, jj++)
+  {
+    float x = (float)ii / (float)nc;
+    float w = Izero(Beta * sqrtf(1.0f - x * x)) / izb; // Kaiser window
+    coeffs_I[jj] = fcf * m_sinc(ii, fcf) * w;
+
+  }
+
+  if (type == 1)
+  {
+    coeffs_I[nc / 2] += 1;
+  }
+  else if (type == 2)
+  {
+    for (int jj = 0; jj < nc + 1; jj++) coeffs_I[jj] *= 2.0f * cosf(PIH * (2 * jj - nc) * fc);
+  }
+  else if (type == 3)
+  {
+    for (int jj = 0; jj < nc + 1; jj++) coeffs_I[jj] *= -2.0f * cosf(PIH * (2 * jj - nc) * fc);
+    coeffs_I[nc / 2] += 1;
+  }
+
+} // END calc_FIR_coeffs
+
+
+
 /*
  * Main
  */
@@ -150,7 +271,7 @@ void app_main(void)
 		.communication_format = I2S_COMM_FORMAT_I2S,
 		.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
 		.intr_alloc_flags = ESP_INTR_FLAG_LEVEL2,
-		.dma_buf_count = 3,
+		.dma_buf_count = 4,
 		.dma_buf_len = I2S_READLEN,
 		.use_apll = 1,
 		.tx_desc_auto_clear = 1,
@@ -181,50 +302,66 @@ void app_main(void)
 	size_t i2s_bytes_read = 0;
 	size_t i2s_bytes_written = 0;
 
-	int16_t i2s_buffer_read[I2S_READLEN / sizeof(int16_t)];
-	int16_t i2s_buffer_write[I2S_READLEN / sizeof(int16_t)];
-int shift = 2; 
-#define BLOCK_SIZE 63
-const int N_BLOCKS = 4; //9; // 6 blocks á 128 samples  = 768 samples. No. of samples has to be dividable by 2 AND by 3 AND by 4 // 6 blocks of 128 samples == 768 samples = 17.4ms
+DMA_ATTR static	int16_t i2s_buffer_read[I2S_READLEN / sizeof(int16_t)];
+DMA_ATTR static	int16_t i2s_buffer_write[I2S_READLEN / sizeof(int16_t)];
+int shift = 4; 
+#define BLOCK_SIZE 128
+const int N_BLOCKS = 6; //9; // 6 blocks á 128 samples  = 768 samples. No. of samples has to be dividable by 2 AND by 3 AND by 4 // 6 blocks of 128 samples == 768 samples = 17.4ms
 const int WINDOW_LENGTH = N_BLOCKS * BLOCK_SIZE;
 const int WINDOW_LENGTH_D_2 = WINDOW_LENGTH / 2;
 #define IN_BUFFER_SIZE WINDOW_LENGTH
 #define HOPSIZE_4 WINDOW_LENGTH/4
 #define HOPSIZE_3 WINDOW_LENGTH/3
-DMA_ATTR static float in_buffer_L[4][252];
-DMA_ATTR static float in_buffer_R[4][252];
+DMA_ATTR static float in_buffer_L[4][768];
+DMA_ATTR static float in_buffer_R[4][768];
 //float in_buffer_L[IN_BUFFER_SIZE];
 //float in_buffer_R[IN_BUFFER_SIZE];
-DMA_ATTR static float out_buffer_L[252];
-DMA_ATTR static float out_buffer_R[252];
-DMA_ATTR static float add_buffer_L[126];
-DMA_ATTR static float add_buffer_R[126];
-DMA_ATTR static float window[252];
+DMA_ATTR static float out_buffer_L[768];
+DMA_ATTR static float out_buffer_R[768];
+DMA_ATTR static float add_buffer_L[384];
+DMA_ATTR static float add_buffer_R[384];
+DMA_ATTR static float window[768];
 int buffer_idx = 0;
 int hop0 = 0;
 int hop1 = 1;
 int hop2 = 2;
 int hop3 = 3;
 
+// apply windowing to array window[] by using function from the esp_dsp lib
+dsps_wind_hann_f32(window, WINDOW_LENGTH);
+//dsps_wind_blackman_harris_f32(window, WINDOW_LENGTH);
+//dsps_wind_blackman_nuttall_f32(window, WINDOW_LENGTH);
 
-  for(unsigned idx=0; idx < WINDOW_LENGTH; idx++)
-  { // von Hann window
-     window[idx] = 0.5f * (1.0f - cosf(TWO_PI * (float)idx / ((float)(WINDOW_LENGTH - 1))));  
-    // Blackman-Nuttall
-    //window[idx] = 0.3635819f - 0.4891775f*cosf(2.0*M_PI*(float)idx/((float)(WINDOW_LENGTH-1))) + 0.1365995*cosf(FOURPI*(float)idx/((float)(WINDOW_LENGTH-1))) - 0.0106411f*cosf(SIXPI*(float)idx/((float)(WINDOW_LENGTH-1)));  
-  }
+DMA_ATTR static fir_f32_t FIR_L;
+DMA_ATTR static fir_f32_t FIR_R;
+DMA_ATTR static float FIR_coeffs[36];
+DMA_ATTR static float FIR_L_delay[36];
+DMA_ATTR static float FIR_R_delay[36];
+DMA_ATTR static int FIR_N_coeffs=36;
 
-//dsps_wind_hann_f32(window[0], WINDOW_LENGTH);
+// calculate coeffs for interpolation lowpass filter
+calc_FIR_coeffs (FIR_coeffs, FIR_N_coeffs, (float)3500, 80, 0, 0.0, 48000);
+
+// initialize FIR filter
+dsps_fir_init_f32(&FIR_L, FIR_coeffs, FIR_L_delay, FIR_N_coeffs);
+dsps_fir_init_f32(&FIR_R, FIR_coeffs, FIR_R_delay, FIR_N_coeffs);
+
+
 
 	/* continuously read data over I2S, pass it through the filtering function and write it back */
 	while (true) {
 		i2s_bytes_read = I2S_READLEN;
-		i2s_read(I2S_NUM, i2s_buffer_read, I2S_READLEN, &i2s_bytes_read, I2S_READLEN / 2);
+		// this is a first trial to read six blocks of audio, process them and then write six blocks back
+
+		for(uint32_t j = 0; j < N_BLOCKS; j++)
+		{
+		i2s_read(I2S_NUM, &i2s_buffer_read, I2S_READLEN, &i2s_bytes_read, 100);
 
 		for (uint32_t i = 0; i < i2s_bytes_read / 4; i++)
 		{
-			in_buffer_L[buffer_idx][i] = (float)i2s_buffer_read[i * 2 + 0] / 32768.0f;
-			in_buffer_R[buffer_idx][i] = (float)i2s_buffer_read[i * 2 + 1] / 32768.0f;
+			in_buffer_L[buffer_idx][i + j * BLOCK_SIZE] = (float)i2s_buffer_read[i * 2 + 0] / 32768.0f;
+			in_buffer_R[buffer_idx][i + j * BLOCK_SIZE] = (float)i2s_buffer_read[i * 2 + 1] / 32768.0f;
+		}
 		}
 
       /**********************************************************************************
@@ -329,20 +466,18 @@ int hop3 = 3;
       // blocksize is BEFORE zero stuffing
       // recycle in_buffer as temporary buffer
       if(shift == 2)
-      {
+      { // zero stuffing		
             for (unsigned i = 0; i < WINDOW_LENGTH_D_2; i++)
             {
-                out_buffer_L[i * 2 + 0] = add_buffer_L[i];
-                out_buffer_L[i * 2 + 1] = 0;
-                out_buffer_R[i * 2 + 0] = add_buffer_R[i];
-                out_buffer_R[i * 2 + 1] = 0;
+                in_buffer_L[buffer_idx][i * 2 + 0] = add_buffer_L[i];
+                in_buffer_L[buffer_idx][i * 2 + 1] = 0;
+                in_buffer_R[buffer_idx][i * 2 + 0] = add_buffer_R[i];
+                in_buffer_R[buffer_idx][i * 2 + 1] = 0;
 	    }
-
-         // arm_fir_interpolate_f32(&interpolation_L, add_buffer_L, in_buffer_L[0], BLOCK_SIZE * N_BLOCKS / (uint32_t)(shift));
-         // arm_fir_interpolate_f32(&interpolation_R, add_buffer_R, in_buffer_R[0], BLOCK_SIZE * N_BLOCKS / (uint32_t)(shift));
-          // do some scaling / gain application after the interpolation
-         // arm_scale_f32(in_buffer_L[0], audio_gain, out_buffer_L, BLOCK_SIZE * N_BLOCKS);
-         // arm_scale_f32(in_buffer_R[0], audio_gain, out_buffer_R, BLOCK_SIZE * N_BLOCKS);
+	// FIR lowpass filter to eliminate aliases caused by zero-stuffing
+	// zero-stuffing & lowpass filtering == interpolation 
+	dsps_fir_f32_ae32(&FIR_L, in_buffer_L[buffer_idx], out_buffer_L, BLOCK_SIZE * N_BLOCKS);
+	dsps_fir_f32_ae32(&FIR_R, in_buffer_R[buffer_idx], out_buffer_R, BLOCK_SIZE * N_BLOCKS);
 
       }
       else if(shift == 3)
@@ -350,38 +485,36 @@ int hop3 = 3;
           // simple zero-stuffing
             for (unsigned i = 0; i < HOPSIZE_3; i++)
             {
-                out_buffer_L[i * 3 + 0] = add_buffer_L[i];
-                out_buffer_L[i * 3 + 1] = 0;
-                out_buffer_L[i * 3 + 2] = 0;
-                out_buffer_R[i * 3 + 0] = add_buffer_R[i];
-                out_buffer_R[i * 3 + 1] = 0;
-                out_buffer_R[i * 3 + 2] = 0;
+                in_buffer_L[buffer_idx][i * 3 + 0] = add_buffer_L[i];
+                in_buffer_L[buffer_idx][i * 3 + 1] = 0;
+                in_buffer_L[buffer_idx][i * 3 + 2] = 0;
+                in_buffer_R[buffer_idx][i * 3 + 0] = add_buffer_R[i];
+                in_buffer_R[buffer_idx][i * 3 + 1] = 0;
+                in_buffer_R[buffer_idx][i * 3 + 2] = 0;
 	    }
-          // arm_fir_interpolate_f32(&interpolation_L, add_buffer_L, in_buffer_L[buffer_idx], BLOCK_SIZE * N_BLOCKS / (uint32_t)(shift));
-          // arm_fir_interpolate_f32(&interpolation_R, add_buffer_R, in_buffer_R[buffer_idx], BLOCK_SIZE * N_BLOCKS / (uint32_t)(shift));
-          // do some scaling / gain application after the interpolation
-          // arm_scale_f32(in_buffer_L[buffer_idx], audio_gain, out_buffer_L, BLOCK_SIZE * N_BLOCKS);
-          // arm_scale_f32(in_buffer_R[buffer_idx], audio_gain, out_buffer_R, BLOCK_SIZE * N_BLOCKS);
+	// FIR lowpass filter to eliminate aliases caused by zero-stuffing
+	// zero-stuffing & lowpass filtering == interpolation 
+	dsps_fir_f32_ae32(&FIR_L, in_buffer_L[buffer_idx], out_buffer_L, BLOCK_SIZE * N_BLOCKS);
+	dsps_fir_f32_ae32(&FIR_R, in_buffer_R[buffer_idx], out_buffer_R, BLOCK_SIZE * N_BLOCKS);
       }
       else if(shift == 4)
       {
-          // arm_fir_interpolate_f32(&interpolation_L, add_buffer_L, in_buffer_L[buffer_idx], BLOCK_SIZE * N_BLOCKS / (uint32_t)(shift));
-          // arm_fir_interpolate_f32(&interpolation_R, add_buffer_R, in_buffer_R[buffer_idx], BLOCK_SIZE * N_BLOCKS / (uint32_t)(shift));
-          // do some scaling / gain application after the interpolation
-          // arm_scale_f32(in_buffer_L[buffer_idx], audio_gain, out_buffer_L, BLOCK_SIZE * N_BLOCKS);
-          // arm_scale_f32(in_buffer_R[buffer_idx], audio_gain, out_buffer_R, BLOCK_SIZE * N_BLOCKS);
           // simple zero-stuffing
             for (unsigned i = 0; i < HOPSIZE_4; i++)
             {
-                out_buffer_L[i * 4 + 0] = add_buffer_L[i];
-                out_buffer_L[i * 4 + 1] = 0;
-                out_buffer_L[i * 4 + 2] = 0;
-                out_buffer_L[i * 4 + 3] = 0;
-                out_buffer_R[i * 4 + 0] = add_buffer_R[i];
-                out_buffer_R[i * 4 + 1] = 0;
-                out_buffer_R[i * 4 + 2] = 0;
-                out_buffer_R[i * 4 + 3] = 0;            
+                in_buffer_L[buffer_idx][i * 4 + 0] = add_buffer_L[i];
+                in_buffer_L[buffer_idx][i * 4 + 1] = 0;
+                in_buffer_L[buffer_idx][i * 4 + 2] = 0;
+                in_buffer_L[buffer_idx][i * 4 + 3] = 0;
+                in_buffer_R[buffer_idx][i * 4 + 0] = add_buffer_R[i];
+                in_buffer_R[buffer_idx][i * 4 + 1] = 0;
+                in_buffer_R[buffer_idx][i * 4 + 2] = 0;
+                in_buffer_R[buffer_idx][i * 4 + 3] = 0;            
 	    }
+	// FIR lowpass filter to eliminate aliases caused by zero-stuffing
+	// zero-stuffing & lowpass filtering == interpolation 
+	dsps_fir_f32_ae32(&FIR_L, in_buffer_L[buffer_idx], out_buffer_L, BLOCK_SIZE * N_BLOCKS);
+	dsps_fir_f32_ae32(&FIR_R, in_buffer_R[buffer_idx], out_buffer_R, BLOCK_SIZE * N_BLOCKS);
       }
 
       /**********************************************************************************
@@ -389,23 +522,18 @@ int hop3 = 3;
        **********************************************************************************/
 
 
-		/* xxx */
+		/* now write the six blocks back to the I2S output stream */
+		for(uint32_t j = 0; j < N_BLOCKS; j++)
+		{
 		for (uint32_t i = 0; i < i2s_bytes_read / 4; i++)
 		{
-			i2s_buffer_write[i * 2 + 0] = (int16_t)(out_buffer_L[i] * 32768.0f * 5.0f);
-			i2s_buffer_write[i * 2 + 1] = (int16_t)(out_buffer_R[i] * 32768.0f * 5.0f);
+			i2s_buffer_write[i * 2 + 0] = (int16_t)(out_buffer_L[i + j * BLOCK_SIZE] * 32768.0f * 5.0f); // apply some audio gain
+			i2s_buffer_write[i * 2 + 1] = (int16_t)(out_buffer_R[i + j * BLOCK_SIZE] * 32768.0f * 5.0f);
+		}
+		i2s_write(I2S_NUM, &i2s_buffer_write, i2s_bytes_read, &i2s_bytes_written, I2S_READLEN / 2);
 		}
 
-
-		/* left channel filter */
-//		for (uint32_t i = 0; i < i2s_bytes_read / 2; i += 2)
-//			i2s_buffer_write[i] = dummyfilter(i2s_buffer_read[i]);
-
-		/* right channel filter */
-//		for (uint32_t i = 1; i < i2s_bytes_read / 2; i += 2)
-//			i2s_buffer_write[i] = dummyfilter(i2s_buffer_read[i]);
-
-		i2s_write(I2S_NUM, i2s_buffer_write, i2s_bytes_read, &i2s_bytes_written, I2S_READLEN / 2);
 	} // end while ("loop")
 } // end main
+
 
